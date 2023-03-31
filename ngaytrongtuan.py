6c93aa2fb92167e2d3b9fc94c1950605a8de90b5
nguyenbaongoc123 <127759913+nguyenbaongoc123@users.noreply.github.com>
2023-03-31 21:35:33 +0900

    Create ngaytrongtuan.py

---
+++ ngaytrongtuan.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+num = int(input())
+if num ==1:
+    print("Monday")
+elif num == 2:
+    print("Tuesday")
+elif num == 3:
+    print("Wednesday")
+elif num == 4:
+    print("Thursday")
+elif num == 5:
+    print("Friday")
+elif num == 6:
+    print("Saturday")
+elif num == 7:
+    print("Sunday")
+else:
+    print("Error, out of range")
+
+
+# In[ ]:
+
+
+
+
